5e18a4a4b8061f8ab6f0d6164692226048b5180a
Jaime Alvarez <jrralvarez@gmail.com>
2021-05-12 21:41:53 -0400

    added a way to get all comments. have the file structure for comment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import Comments from './components/Comments/Comments';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      videoInfo: []
+    }
   }
 
   componentDidMount(){
-
+    axios.get("http://localhost:5000/api/comments/")
+    .then(res => {
+      console.log('get all comments', res);
+      const info = res.data;
+      this.setState({
+        videoInfo: info
+      })
+    })
+      .catch(err => {
+        console.log(err)
+    })
   }
-  
+
   render() { 
-    return (  );
+    return ( 
+      <div>
+        <h1> Hello World! </h1>
+        <Comments />
+      </div>
+    );
   }
 }
  
---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Comment from '../Comment/Comment';
+
+class Comments extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Where the comment will be.</h1>
+                <Comment title='Comment' />
+            </div>
+        )
+    }
+}
+
+export default Comments;
