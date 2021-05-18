3cceb26801c4a92114f9644b7ac4fcb027df6671
Daniel Ramirez <notused@notused.net>
2021-05-18 10:38:09 -0700

    Adds test data to avoide overload on API keys

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,39 +1,36 @@
 import Player from "../Player/Player";
 import CommentContainer from "../CommentContainer/CommentContainer";
 import RelatedVideos from "../RelatedVideos/RelatedVideos";
-import ShowComments from '../ShowComments/ShowComments';
+import ShowComments from "../ShowComments/ShowComments";
 
 const MainView = (props) => {
   return (
     <div className="container">
       <div className="row row-cols-2">
-        <div className="col-10">
+        <div className="col-8">
           <Player
             key={props.videoId}
             id={props.videoId}
-            width="850"
+            width="650"
             height="450"
-            link={`http://www.youtube.com/embed/${props.videoId}?enablejsapi=1&origin=http://example.com`}
+            link={`http://www.youtube.com/embed/?enablejsapi=1&origin=http://example.com`}
             // link={`https://www.googleapis.com/youtube/v3/search?q=${props.videoId}&key=${apiKey}}`}
           />
         </div>
-        <div className="col-2">
+        <div className="col-4">
           <RelatedVideos relatedVideos={props.relatedVideosData} />
         </div>
       </div>
       <div className="row row-cols-1">
-        <div className ='row'>
-          <ShowComments 
-          currentVideo={props.videoId}
-          commentInfo={props.commentInfo}
-          />
+        <div className="row">
+          <ShowComments currentVideo={props.videoId} commentInfo={props.commentInfo} />
         </div>
-          <CommentContainer
-            handleSubmit={(e) => props.handleSubmit(e)}
-            handleChange={(e) => props.handleChange(e)}
-            postComments={props.postComments}
-            text={props.text}
-            videoId={props.videoId}
+        <CommentContainer
+          handleSubmit={(e) => props.handleSubmit(e)}
+          handleChange={(e) => props.handleChange(e)}
+          postComments={props.postComments}
+          text={props.text}
+          videoId={props.videoId}
         />
       </div>
     </div>
