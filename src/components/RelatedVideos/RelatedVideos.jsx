81bc1715b6e41f12c36edfb715decf8aa96c7681
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-24 10:35:04 -0500

    centered the videos when the screen is max width.

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 
 const RelatedVideos = (props) => {
-  const relatedVideos = props.relatedVideos.items;
-  return (
-    <div className="container justify-content-center">
-      <div className="column">
-        {relatedVideos.map((video) => (
-          <div key={video.id.videoId} className="thumbnail">
-            <button className="btn btn-dark col" onClick={() => props.updateActiveVideo(video)}>
-              <img src={video.snippet.thumbnails.medium.url} width="200" height="100" />
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const relatedVideos = props.relatedVideos.items;
+	return (
+		<div className="container justify-content-center">
+			<div className="column">
+				{relatedVideos.map((video) => (
+					<div key={video.id.videoId} className="thumbnail">
+						<button
+							className="btn btn-dark col"
+							onClick={() => props.updateActiveVideo(video)}>
+							<img
+								src={video.snippet.thumbnails.medium.url}
+								width="200"
+								height="100"
+							/>
+						</button>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default RelatedVideos;
--- src/components/SearchResultsContainer/SearchResultsContainer.jsx
+++ src/components/SearchResultsContainer/SearchResultsContainer.jsx
@@ -1,46 +1,50 @@
 import React from "react";
+import "./SearchResultsContainer.css";
 
 const SearchResultsContainer = (props) => {
-  const videos = props.videos.items;
-  const nextIndex = props.videos.nextPageToken;
-  const previousIndex = props.videos.prevPageToken;
-  return (
-    <div className="container">
-      <div className="row row-cols-3 justify-content-center">
-        <div className="col-1 indexButton">
-          <button className="btn btn-dark" onClick={() => props.changePage(previousIndex)}>
-            Back
-          </button>
-        </div>
-        <div className="col-10">
-          <div className="row row-cols-auto justify-content-center">
-            {videos.map((video) => (
-              <div className="col" key={video.id.videoId}>
-                <button
-                  key={video.id.videoId}
-                  className="btn btn-dark col"
-                  onClick={() => props.setPlayer(video)}
-                >
-                  <img
-                    // src={`https://img.youtube.com/vi/${video.id.videoId}/mqdefault.jpg`}
-                    src={video.snippet.thumbnails.medium.url}
-                    width="285"
-                    height="150"
-                  />
-                </button>
-                <p className="text-center">{video.snippet.title}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="col-1 indexButton">
-          <button className="btn btn-dark" onClick={() => props.changePage(nextIndex)}>
-            More Videos
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	const videos = props.videos.items;
+	const nextIndex = props.videos.nextPageToken;
+	const previousIndex = props.videos.prevPageToken;
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-1 indexButton">
+					<button
+						className="btn btn-dark"
+						onClick={() => props.changePage(previousIndex)}>
+						Back
+					</button>
+				</div>
+				<div className="col-10">
+					<div className="row">
+						{videos.map((video) => (
+							<div className="col-6" key={video.id.videoId}>
+								<button
+									key={video.id.videoId}
+									className="btn btn-dark video-layout"
+									onClick={() => props.setPlayer(video)}>
+									<img
+										// src={`https://img.youtube.com/vi/${video.id.videoId}/mqdefault.jpg`}
+										src={video.snippet.thumbnails.medium.url}
+										width="285"
+										height="150"
+									/>
+								</button>
+								<p className="text-center">{video.snippet.title}</p>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="col-1 indexButton">
+					<button
+						className="btn btn-dark"
+						onClick={() => props.changePage(nextIndex)}>
+						More Videos
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SearchResultsContainer;
